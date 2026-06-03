7d27e1825c6c82eae0cdb7b7e826b90cb2261d85
Dongmin (Dennis) Kim <dongminkim09@gmail.com>
2026-06-03 11:05:17 -0500

    Update 11_Model_fit.R
    updated the current model fits.

--- Scripts4publication/11_Model_fit.R
+++ Scripts4publication/11_Model_fit.R
@@ -16,16 +16,16 @@ library(ggplot2)
 
 genPath <- "/path_to_folder/"
 
-final_df <- readRDS(here::here(genPath, "Final_dryland_data_mar2026.rds"))
+final_df <- readRDS(here::here(genPath, "Final_dryland_data_may2026.rds"))
 
-# 1. Finalized Model format
-vulture_model <- bam(
+# 1. model: no interaction terms
+model1 <- bam(
   dsl_UD ~
     # Fixed effects:
     # Species-specific intercepts (baseline UD differences)
     # and REALM-level differences shared across species
     # and dryland-level differences shared across species
-    species + REALM+ HIX_DESC+
+    species + REALM+ wmodeDrylands+
     
     # Environmental predictors:
     # Species-specific smooths allow each species
@@ -58,22 +58,62 @@ vulture_model <- bam(
   discrete = TRUE     # good for large datasets
 )
 
-# 2. Finalized Model format: dryland
-dryland_model <- bam(
+# 2. Finalized Model format: dryland and REALM
+model2 <- bam(
   dsl_UD ~
     # Fixed effects:
     # Species-specific intercepts (baseline UD differences)
     # and REALM-level differences shared across species
     # and dryland-level differences shared across species
-    species + REALM+ HIX_DESC+
+    species + REALM+ wmodeDrylands+
     
     # Environmental predictors:
     # Species-specific smooths allow each species
     # to respond differently (nonlinearly) to NDVI
-    s(dsl_wmNDVI, by = interaction(species, HIX_DESC),  k = 5) +
+    s(dsl_wmNDVI, by = interaction(species, wmodeDrylands),  k = 5) +
     
     # Species-specific nonlinear response to livestock density
-    s(wmLifestock, by = interaction(species, HIX_DESC),  k = 5) +
+    s(wmLifestock, by = interaction(species, wmodeDrylands),  k = 5) +
+    
+    # Sampling effort correction:
+    # Nonlinear effect of number of locations per month,
+    # allowed to differ by species (NOT a random effect)
+    # This controls for effort-induced bias in UD estimation
+    s(totNbLocs, by = species, k =5)+
+    
+    # Spatial structure:
+    # 2D smooth over centroid longitude/latitude,
+    # with separate spatial surfaces for each species
+    # (accounts for broad-scale spatial autocorrelation)
+    s(UDwMeanLongitude, UDwMeanLatitude, by = species, k = 100) +
+    
+    # Individual-level random intercept:
+    # Accounts for repeated measures within animals
+    # (IDs can repeat across species)
+    s(animal_id, bs = "re"),
+  
+  data = final_df,
+  family = gaussian(),
+  method = "fREML",   # stable & efficient
+  discrete = TRUE     # good for large datasets
+)
+
+# 2. Finalized Model format: dryland only
+model3 <- bam(
+  dsl_UD ~
+    # Fixed effects:
+    # Species-specific intercepts (baseline UD differences)
+    # and REALM-level differences shared across species
+    # and dryland-level differences shared across species
+    species + wmodeDrylands+
+    
+    # Environmental predictors:
+    # Species-specific smooths allow each species
+    # to respond differently (nonlinearly) to NDVI
+    s(dsl_wmNDVI, by = interaction(species, wmodeDrylands),  k = 5) +
+    
+    # Species-specific nonlinear response to livestock density
+    s(wmLifestock, by = interaction(species, wmodeDrylands),  k = 5) +
     
     # Sampling effort correction:
     # Nonlinear effect of number of locations per month,
@@ -99,5 +139,6 @@ dryland_model <- bam(
 )
 
 # save the final model and modified data frame 
-saveRDS(dryland_model, file="dryland_model_mar2026.rds")
-saveRDS(vulture_model, file = "vulture_model_mar2026.rds")
+saveRDS(model1, file="model1_may2026.rds")
+saveRDS(model2, file = "model2_may20262026.rds")
+saveRDS(model3, file = "model3_may2026.rds")
